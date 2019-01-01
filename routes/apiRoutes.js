1e4ed6c9c78b58c71d7f9dcbecf88baf5670f71e
Alexander Osborn <misterzander@gmail.com>
2019-01-01 17:23:38 -0600

    I cannot believe that actually worked

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const cheerio = require("cheerio");
+const axios = require("axios");
 
 //Model
 const Article = require("../models/Article");
@@ -16,6 +18,27 @@ router.get("/", (req, res) => {
       //You will recieve an empty array if there's nothing to display
       return res.status(200).json(articles);
     });
+});
+
+router.get("/scrape", (req, res) => {
+  //Make a request to NHL
+  axios.get("https://www.nhl.com/").then(response => {
+    const $ = cheerio.load(response.data);
+    const scrapings = [];
+    $("li.mixed-feed__item--article").each((i, element) => {
+      let deets = {};
+      deets.contentId = $(element).data("content-id");
+      deets.headline = $(element)
+        .children("div.mixed-feed__item-header")
+        .children("div.mixed-feed__item-header-text")
+        .children("a")
+        .attr("href");
+      scrapings.push(deets);
+    });
+    console.log(scrapings);
+    console.log(`Final Tally: ${scrapings.length} articles`);
+    return res.status(200).json({ msg: "Done" });
+  });
 });
 
 module.exports = router;
